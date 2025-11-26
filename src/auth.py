943e2bad5ddbe9388a779643f146ac110c22b914
opensource-lover <9l.schastliv@gmail.com>
2025-11-26 17:53:59 +0300

    completed the basic auth following the docs

--- src/auth.py
+++ src/auth.py
@@ -1,21 +1,92 @@
-from fastapi import APIRouter, Depends, HTTPException
+import jwt
+from pwdlib import PasswordHash
+from datetime import datetime, timedelta, timezone
+
+from typing import Annotated
 from sqlalchemy.orm import Session
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+
+from core.config import settings
 from db.database import get_db
 from db.models import User
-# passlib has an issue, that is a fix I found. consider using alternatives:
-import bcrypt
-bcrypt.__about__ = bcrypt
-from passlib.context import CryptContext
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from jose import jwt, JWTError
-from core.config import settings
 from schemas import UserCreate, Token
 
+auth_router = APIRouter(prefix="/auth", tags=["authentication"])
 
-# router = APIRouter(prefix='/auth', tags=['auth'])
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/token")
 
-# TODO learn what this is:
-bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+password_hash = PasswordHash.recommended()
 
-oauth2_bearer = OAuth2PasswordBearer(tokenUrl='/auth/token')
+def verify_password(plain_password, hashed_password):
+    return password_hash.verify(plain_password, hashed_password)
+
+def hash_password(password):
+    return password_hash.hash(password)
+
+def create_access_token(data: dict, expires_delta: timedelta):
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + expires_delta
+    to_encode.update({'exp': expire})
+
+    return jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+
+def authenticate_user(
+    email: str, 
+    password: str, 
+    db: Session
+):
+    db_user = db.query(User).filter(User.email == email).first()
+    if not db_user or not verify_password(password, db_user.password_hashed):
+        return False
+
+    return db_user
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme), 
+    db: Session = Depends(get_db)
+) -> User:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        email: str = payload.get('sub')
+        if not email:
+            print("NO EMAIL")
+            raise credentials_exception
+
+    except jwt.PyJWTError:
+        print("JWT EXCEPT")
+        raise credentials_exception
+
+    user = db.query(User).filter(User.email == email).first()
+    if not user:
+        print("NO USER")
+        raise credentials_exception
+
+    return user
+
+@auth_router.post("/token")
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    db: Session = Depends(get_db)
+):
+    # OAuth2PasswordRequestForm has strict fields names, but we will treat username as an email
+    user = authenticate_user(form_data.username, form_data.password, db)
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.email}, expires_delta=access_token_expires
+    )
+    return Token(access_token=access_token, token_type="bearer")
 
